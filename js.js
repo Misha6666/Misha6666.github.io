6d74c469204aa0244820f7d66b50eecb880b465f
Misha6666 <75987160+Misha6666@users.noreply.github.com>
2024-11-19 21:28:15 +0400

    Update js.js

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	window.location.assign = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+	window.location.assign('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 })
